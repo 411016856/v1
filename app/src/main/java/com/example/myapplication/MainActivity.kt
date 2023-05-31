e7324f9a7683ca164a5aa0b5a2601b0296d7f190
411016856 <s1101685@gm.pu.edu.tw>
2023-05-31 11:31:56 +0800

    第三次

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.example.myapplication
 
-import android.graphics.Color
+import android.graphics.Bitmap
+
+import android.graphics.Paint
+import android.graphics.RectF
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
@@ -13,13 +17,18 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.semantics.Role.Companion.Image
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.example.myapplication.ui.theme.MyApplicationTheme
-import com.google.android.filament.Box
+
+import java.nio.file.Files.size
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -37,35 +46,38 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
+    funch()
+    Box(
+
+        modifier = Modifier.fillMaxSize()
+
+    ){
+
+        Canvas(modifier = Modifier){
+
+            drawRect(Color.Blue,  Offset(800f, 200f),Size(30f,30f))
+            drawRect(Color.Blue,  Offset(1620f, 920f),Size(30f,30f))
+        }
+
+    }
+}
+@Composable
+fun funch() {
     Column() {
-        Text(text = "作者: $name")
-        Image(
+        Text(text = "作者: 資管二B 洪詳華")
+        Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.map),
+                contentDescription = "地圖",
+            )
+        }
 
-            painter = painterResource(id = R.drawable.map),
-
-            contentDescription = "地圖",
-
-            alpha = 0.7f,
-
-            modifier = Modifier
-        )
     }
 
-
-//    Box(
-//
-//        modifier = Modifier.fillMaxSize()
-//
-//    ){
-//
-//        Canvas(modifier = Modifier){
-//
-//            drawRect(Color.BLUE,Offset(x = 500,y=500), size())
-//
-//        }
-//
-//    }
-}
+    }
 
 
 @Preview(showBackground = true)
